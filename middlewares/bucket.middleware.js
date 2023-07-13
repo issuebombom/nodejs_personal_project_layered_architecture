e0f171a5cfbb0e38fbdbc688aa42878a26f88631
issuebombom <issuebombom@gmail.com>
2023-07-13 20:44:38 +0900

    fix: using upload img name by uuidv4
    - 한글 이름 사진 업로드 시 글자 꺠짐 및 파일 이름 길이 문제 발생
    - 업로딩 날짜 + uuidv4 조합으로 이름 생성후 업로드

--- middlewares/bucket.middleware.js
+++ middlewares/bucket.middleware.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 const s3 = new AWS.S3({
   accessKeyId: process.env.S3_ACCESS_KEY,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
@@ -14,25 +15,26 @@ class UploadBucket {
   }
 
   // 버킷 업로드 틀
-  upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: process.env.S3_BUCKET_NAME,
-      shouldTransform: true, // 뭘 트랜스폼 한다는거지?
-      key: (req, file, callback) => {
-        callback(null, `${this.profileImageDir}/${Date.now()}_${file.originalname}`);
-      },
-      acl: 'public-read-write',
-      limits: { fileSize: 5 * 1024 * 1024 }, // 이미지 용량을 5MB이하로 제한
-      contentType: multerS3.AUTO_CONTENT_TYPE, // 파일의 Content-Type 자동 설정
-      metadata: (req, file, callback) => {
-        callback(null, { fieldName: file.fieldname });
-      },
-    }),
-  });
+  upload = (saveDir) =>
+    multer({
+      storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_BUCKET_NAME,
+        shouldTransform: true, // 뭘 트랜스폼 한다는거지?
+        key: (req, file, callback) => {
+          callback(null, `${saveDir}/${Date.now()}_${uuidv4()}`);
+        },
+        acl: 'public-read-write',
+        limits: { fileSize: 5 * 1024 * 1024 }, // 이미지 용량을 5MB이하로 제한
+        contentType: multerS3.AUTO_CONTENT_TYPE, // 파일의 Content-Type 자동 설정
+        metadata: (req, file, callback) => {
+          callback(null, { fieldName: file.fieldname });
+        },
+      }),
+    });
 
   // 프로필 사진 버킷 업로드
-  profileImage = (targetFile) => this.upload.single(targetFile);
+  profileImage = (targetFile) => this.upload(this.profileImageDir).single(targetFile);
 }
 
 module.exports = UploadBucket;
